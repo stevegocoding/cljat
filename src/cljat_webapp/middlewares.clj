6dd1232cf7c54f4186b4d1b4c61a2924c3887ff9
stevegocoding <stevegocoding@gmail.com>
2016-04-09 19:03:19 -0400

    handle expired token

--- src/cljat_webapp/middlewares.clj
+++ src/cljat_webapp/middlewares.clj
@@ -1,12 +1,20 @@
 (ns cljat-webapp.middlewares
   (:require [ring.util.response :refer [status response content-type not-found redirect set-cookie]]
-            [cljat-webapp.auth :refer :all]))
+            [cljat-webapp.auth :refer :all]
+            [clojure.tools.logging :as log]))
 
 (defn wrap-auth-token-cookie [handler pubkey]
   (fn [req]
     (let [user-info (when-let [token (-> req :cookies (get "cljat-token") :value)]
-                      (unsign-token token pubkey))]
-      (handler (assoc req :identity (:uid user-info))))))
+                      (try
+                        (unsign-token token pubkey)
+                        (catch clojure.lang.ExceptionInfo e
+                          (do
+                            (log/info "Token has expired!")
+                            nil))))]
+      (if user-info
+        (handler (assoc req :identity (:uid user-info)))
+        (handler req)))))
 
 (defn wrap-authentication [handler]
   (fn [req]
