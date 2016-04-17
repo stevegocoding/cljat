b17a9329485b6222640c291deba19bccc34dc539
stevegocoding <stevegocoding@gmail.com>
2016-04-16 23:56:04 -0400

    sql for creating new thread

--- src/cljat_webapp/model.clj
+++ src/cljat_webapp/model.clj
@@ -1,5 +1,7 @@
 (ns cljat-webapp.model
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as sql]
+            [clj-time.core :as t]
+            [clj-time.coerce :as tc]))
 
 (defn find-user-by-email [db email]
   (sql/query (:conn db)
@@ -18,6 +20,14 @@
 
 (defn add-friendship [db user-id friend-id]
   (sql/insert! (:conn db) :friendships {:user_id user-id :friend_id friend-id} {:user_id friend-id :friend_id user-id}))
+
+(defn add-thread [db user-id friend-id]
+  (sql/with-db-transaction [conn (:conn db)]
+    (let [new-thread-id (->
+                          (sql/insert! conn :threads {:title "chat default" :created_time (tc/to-sql-time (t/now))})
+                          (first) (vals) (first))]
+      (sql/insert! conn :users_threads {:user_id user-id :thread_id new-thread-id})
+      (sql/insert! conn :users_threads {:user_id friend-id :thread_id new-thread-id}))))
 
 (defn find-threads-by-user-id [db user-id]
   (sql/query (:conn db)
